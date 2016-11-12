cef2e6ab9356d5a0255106deff379531f95dc232
AngelAlvie <alvareza@mit.edu>
2016-11-11 19:46:04 -0500

    Added simple search functionality.

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -5,20 +5,3 @@
 * does not work right now, I will put more serious work into it over weekend.
 *
 */
-
-console.log("script running");
-
-  var searchFields = $('.project');
-
-function search() {
-  var search = $('searchBar');
-
-  for (project in searchFields)
-
-  if(searchBar.value === searchFields[project].get(2)) {
-    searchFields[project].show();
-  } else {
-    searchFields[project].hide();
-  }
-
-}
