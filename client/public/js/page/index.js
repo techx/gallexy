05e88f5f246781e717b8d522c430f0b0f1e61803
AngelAlvie <alvareza@mit.edu>
2018-01-01 20:18:18 -0800

    Made index page retrieve data from the API.

--- client/public/js/page/index.js
+++ client/public/js/page/index.js
@@ -1,11 +1,12 @@
 // TODO: implement sort dropdown
 console.log('Index Page JavaScript Loaded');
 
-localProjects = []; //list of projects that we already have
+localProjects = []; //list of ALL projects that we already have
+renderedProjects = [];
 
-let projectType = 'projX';
+let projectType = 'any';
 let projectOrder = 'popular';
-
+let projectContainer = $('#project-container');
 $(document).ready(() => {
   $('.ui.dropdown').dropdown({
     on: 'hover',
@@ -17,27 +18,120 @@ $(document).ready(() => {
       console.log('changed project type');
       changeColor(text);
       projectType = text;
-      $.ajax({
-        url:'/api/search',
-        method:'GET',
-        data: {
-          projectType: projectType,
-          projectOrder: projectOrder,
-          filters: [] 
-        },
-        success: function(data, status, jqXHR) {
-          console.log("successful API call");
-          console.log(data);
-        }
-      });
+      getProjects();
     }
   });
   $('#project-order').dropdown({
     onChange: function (text, value) {
-      console.log('changed project ordering');
+      console.log('changed project ordering to '+ text);
       projectOrder = text;
+      render();
     }
   });
-  //include transactions to request more data about particular projects
   
+  // get Projects on load
+  getProjects();
 });
+
+function getProjects() {
+  $.ajax({
+    url: '/api/search',
+    method: 'GET',
+    data: {
+      projectType: projectType,
+      projectOrder: projectOrder,
+      filters: []
+    },
+    success: function (data, status, jqXHR) {
+      localProjects = data;
+      render();
+    }
+  });
+}
+
+function render() {
+  sortAndFilterLocal();
+  projectContainer.html("");
+  renderedProjects.forEach(element => {
+    projectContainer.append(createProjectCard(element));  
+  });
+}
+
+function sortAndFilterLocal() {
+  if (projectType != "any") {
+    renderedProjects = localProjects.filter((project) => {
+      return project.projectType == projectType;
+    });
+  } else {
+    renderedProjects = localProjects.slice();;
+  }
+
+  console.log(projectOrder);
+  if (projectOrder == "random") {
+    renderedProjects = randomSort(renderedProjects);
+  } else if (projectOrder == "popular") {
+    renderedProjects = popularSort(renderedProjects);
+  } else if (projectOrder == "recent") {
+    renderedProjects = recentSort(renderedProjects);
+  } else {
+    console.log("no sort applied");
+  }
+}
+
+// cmp is true when greater, false otherwise
+function qsort(arr, cmp) {
+  if (arr.length == 0) {
+    return [];
+  } else {
+    let pivot = arr.shift();
+    let left = [];
+    let right = [];
+    arr.forEach(element => {
+      if (cmp(pivot, element)) {
+        right.push(element);
+      } else {
+        left.push(element);
+      }
+    });
+    return qsort(left, cmp).concat([pivot]).concat(qsort(right, cmp))
+  }
+}
+
+function randomSort(arr) {
+  return qsort(arr, (a, b) => {
+    return Math.random() < 0.5;
+  });
+}
+function popularSort(arr) {
+  return qsort(arr, (a, b) => { 
+    return a.popularity < b.popularity;
+  });
+}
+function recentSort(arr) {
+  return qsort(arr, (a, b) => { 
+    return a.lastChange > b.lastChange;
+  });
+}
+
+function createProjectCard(project) {
+  let html = `<a class = "ui orange card centered" href="/project?id=${project.id}"><div class = "image">`;
+  if (project.picURL) {
+    html += `<img src="${project.picURL}">`;
+  } else {
+    html += `<img src = "/img/placeholder.png">`;
+  }
+  html += `</div><div class="content"><div class="header">${project.title}</div>`;
+  if (project.author) {
+    html += `<div class="meta"><span class="author">${project.author}</span></div>`;
+  }
+  html += `
+        <div class="description">
+          <p>${project.description}</p>
+        </div>
+      </div>
+      <div class="extra content">
+        <p>${project.status}</p>
+      </div>
+    </a>`;
+  return html
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -18,112 +18,15 @@ router.get('/', (req, res, next) => {
       title: 'GalleXy',
       loggedIn:true,
       isAdmin: req.user.admin,
-      projects:[
-        {picURL:"http://www.clockworktalent.com/wp-content/uploads/2015/06/Wearable-tech.jpg",
-          title:"Project Melancholy",
-         author: "Chet Oswald",
-         description: "I am trying to create a wearable that can detect emotions, using analytics and artificial intelligence on an array of sensory inputs.",
-         status:"Started December 2015",
-         id: "189265"
-        },
-        {picURL:"https://www.designnews.com/sites/default/files/desk-features.jpg",
-          title:"Studio Desk Revamp",
-         author: "Claire onyx",
-         description: "I am incorporating a bit of augmented reality into my desk, by adding projectors and AR optical markers.",
-         status:"Started June 2017",
-         id: "189265"
-        },
-        {picURL:"https://github.com/AngelAlvie/Tetris-3D/blob/master/3DTetris/resources/logo.png?raw=true",
-         title:"Tetris 3D",
-         author: "Angel Alvarez",
-         description: "A 3D version of the classic game of Tetris, using Java Swing.",
-         status:"Completed May 2016",
-         id: "189265"
-        },
-        {title:"Title",
-         author: "Author",
-         description: "This is an example project, this is where a description of the project goes.",
-         status:"Project Status.",
-         id: "189265"
-        },
-        {picURL:"https://www.montereybayaquarium.org/-/m/images/animal-guide/marine-mammals/sea-otter-mom-pup.jpg?mh=916&mw=1222&usecustomfunctions=1&centercrop=1",
-         title:"Sea Otter Solidarity Project",
-         author: "Emmet Otterton",
-         id: "189265",
-         description: "We aim to bring otters back into society as a force for social good.",
-         status:"Completed March 2010"
-        },
-        {picURL:"https://blog.therestaurantzone.com/wp-content/uploads/2017/01/restaurant-recruiting-tools.jpg",
-         title:"United Makerspace",
-         author: "James Jasonson",
-         description: "For too long, course 6ers and mechies have been divided. We want to open this makerspace to heal this divide.",
-         status: "Started November 2016",
-         id: "189265"
-        }
-      ]
+      projects:[]
     });
   }, (req, res) => {
     res.render('index', {
       title: 'GalleXy',
       loggedIn:false,
       isAdmin:false,
-      projects:[
-        {picURL:"http://www.clockworktalent.com/wp-content/uploads/2015/06/Wearable-tech.jpg",
-          title:"Project Melancholy",
-         author: "Chet Oswald",
-         description: "I am trying to create a wearable that can detect emotions, using analytics and artificial intelligence on an array of sensory inputs.",
-         status:"Started December 2015",
-         id: "189265"
-        },
-        {picURL:"https://www.designnews.com/sites/default/files/desk-features.jpg",
-          title:"Studio Desk Revamp",
-         author: "Claire onyx",
-         description: "I am incorporating a bit of augmented reality into my desk, by adding projectors and AR optical markers.",
-         status:"Started June 2017",
-         id: "189265"
-        },
-        {picURL:"https://github.com/AngelAlvie/Tetris-3D/blob/master/3DTetris/resources/logo.png?raw=true",
-         title:"Tetris 3D",
-         author: "Angel Alvarez",
-         description: "A 3D version of the classic game of Tetris, using Java Swing.",
-         status:"Completed May 2016",
-         id: "189265"
-        },
-        {picURL:"https://blog.therestaurantzone.com/wp-content/uploads/2017/01/restaurant-recruiting-tools.jpg",
-         title:"United Makerspace",
-         author: "James Jasonson",
-         description: "We wanted to create a makerspace that would be as much a workshop as it was a place for people to meetup.",
-         status: "Started november 2016",
-         id: "189265"
-       },
-        {title:"Title",
-         author: "Author",
-         description: "Description",
-         status:"Not finished",
-         id: "189265"
-        },
-        {picURL: "http://www.jeffgeerling.com/sites/jeffgeerling.com/files/raspberry-pi-dramble-cluster_1.jpg",
-          title:"Title",
-         author: "Author",
-         description: "Description",
-         status:"Not finished",
-         id: "189265"
-        },
-        {picURL: "https://www.3dhubs.com/s3fs-public/talk/attachments/3D_PRINTED_EXOSKELETON_HAND_large.png",
-          title:"Title",
-         author: "Author",
-         description: "Description",
-         status:"Not finished",
-         id: "189265"
-        },
-        {picURL:"https://www.montereybayaquarium.org/-/m/images/animal-guide/marine-mammals/sea-otter-mom-pup.jpg?mh=916&mw=1222&usecustomfunctions=1&centercrop=1",
-         title:"Sea Otter Solidarity Project",
-         author: "Emmet Otterton",
-         description: "We aim to bring otters back into society as a force for social good.",
-         status:"Completed 2010",
-         id: "189265"
-        }
-      ] });
+      projects:[]
+    });
   });
 });
 
